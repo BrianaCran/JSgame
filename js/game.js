00203dccd0e38fa38c5c7e54f9e660f807d32301
BrianaCran <briana.marie.crandall@gmail.com>
2016-12-19 13:09:07 -0500

    flee option completed

--- js/game.js
+++ js/game.js
@@ -34,28 +34,78 @@ function gameConsequence(){
  		 var userChoice = $('.js-answer').val();
      var userChoiceConform = userChoice.toUpperCase();
      game.userChoice = userChoiceConform;
-     game.lastUserChoice = userChoice;
      game.feedback = result(userChoice);
-     game.nextUserChoice = result(userChoice);
 
      displayGameState(game);
      $('.js-answer').val(null);
  	 });
 };
+function resetOptions(){
+  $('#jsOptionOne').text("");
+  $('#jsOptionTwo').text("");
+  $('#jsOptionThree').text("");
+};
 function result(userChoice){
  if(game.userChoice === "FLEE"){
-   return "Are you a fast runner?";
+   $('.js-feedback').text("Are you fast?")
+   $('#jsOptionOne').text("Very fast");
+   $('#jsOptionTwo').text("Not fast");
+   $('#jsOptionThree').text("");
    }
- else if (game.lastUserChoice === "FLEE" && game.nextUserChoice === "Yes"){
-   return "You have escaped!";
+ else if(game.lastUserChoice === "VERY FAST"){
+   $('.js-feedback').text("You have escaped!");
+   resetOptions();
+ }
+ else if(game.lastUserChoice === "NOT FAST"){
+   $('.js-feedback').text("Are you a quiet runner?")
+   $('#jsOptionOne').text("Of Course");
+   $('#jsOptionTwo').text("Not really");
+   $('#jsOptionThree').text("");
+ }
+ else if(game.lastUserChoice === "OF COURSE"){
+   $('.js-feedback').text("You have managed to sneak way from the still sleeping dragon!");
+   resetOptions();
+ }
+ else if(game.lastUserChoice === "NOT REALLY"){
+   $('.js-feedback').text("The dragon has awakened and wasted no time in devouring you!!");
+   resetOptions();
  }
  else{
-   return "invaild answer";
+   $('.js-feedback').text("invaild answer");
  };
- if(game.userChoice === "STEAL"){
-   return "Are you silent?";
- }
 
+
+ if(game.userChoice === "STEAL") {
+   $('.js-feedback').text("Are you silent like a shadow?")
+   $('#jsOptionOne').text("Yup ninja silent");
+   $('#jsOptionTwo').text("Super Clumsy");
+   $('#jsOptionThree').text("");
+ }
+ else if(game.userChoice === "YUP NINJA SILENT"){
+   $('.js-feedback').text("How much treasure are you stealing?")
+   $('#jsOptionOne').text("A wagon full");
+   $('#jsOptionTwo').text("Small bag full");
+   $('#jsOptionThree').text("A handful");
+ }
+ else if (game.userChoice === "A WAGON FULL") {
+   $('.js-feedback').text("You took way to long....the dragon is not please...you've been burnt to a crisp.")
+   resetOptions();
+ }
+ else if (game.userChoice === "SMALL BAG FULL") {
+   $('.js-feedback').text("The dragon stirs...")
+   $('#jsOptionOne').text("Stand still");
+   $('#jsOptionTwo').text("Run for it");
+   $('#jsOptionThree').text("");
+
+ }
+ else if (game.userChoice === "STAND STILL") {
+   $('.js-feedback').text("As you wait with bated breath the dragon continues to slumber. You leave while you still can...")
+   resetOptions();
+ }
+ else if (game.userChoice === "STAND STILL") {
+   $('.js-feedback').text("As you wait with bated breath the dragon continues to slumber. You leave while you still can...")
+   resetOptions();
+ }
 }
 
 
